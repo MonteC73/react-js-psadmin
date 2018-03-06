003856a8ab60c95fa4266a6927cc6c7c276a3244
MonteC73 <jarekucinski@gmail.com>
2018-03-06 23:22:36 +0100

    React Redirect

--- src/components/homePage.js
+++ src/components/homePage.js
@@ -1,6 +1,8 @@
 "use strict";
 
 var React = require('react');
+var Router = require('react-router');
+var Link = Router.Link;
 
 var Home = React.createClass( {
     render: function() {
@@ -8,6 +10,7 @@ var Home = React.createClass( {
             <div className="jumbotron">
                 <h1>Pluralsight Administration</h1>
                 <p>React, Flux, and React Router combined with Browserify, Gulp, and Bootstrap.</p>
+                <Link to="about" className="btn btn-primary btn-lg">Learn more</Link>
             </div>
         );
     }
